ab91e5077324658ef01fa6d83f5cc85f04771f37
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-03-19 15:53:08 -0400

    update notebooks

--- examples/demo_ephys.py
+++ examples/demo_ephys.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import FactorAnalysis as factan
 from pybasicbayes.util.text import progprint_xrange
 from pylds.util import random_rotation
-from pyslds.models import DefaultSLDS
+from pyslds.models import DefaultSLDS, HMMSLDS
+import numpy.random as npr
+from pybasicbayes.distributions import DiagonalRegression, Gaussian, Regression
 
 mat_contents = sio.loadmat('/Users/dalin/Documents/My_Research/Dataset/ALM_Svoboda_Lab_Data/Code/TLDS/TempDat/Simultaneous_Spikes.mat')
 nDataSet = mat_contents['nDataSet']
@@ -32,41 +34,96 @@ xDim = 5
 inputDim = 1 # some constants
 inputs = np.ones((numTime, inputDim))
 
-estimator = factan(n_components=xDim, tol=0.000001, copy=True, 
-                   max_iter=1000, noise_variance_init=None, 
-                   svd_method='randomized', iterated_power=3, 
+estimator = factan(n_components=xDim, tol=0.000001, copy=True,
+                   max_iter=1000, noise_variance_init=None,
+                   svd_method='randomized', iterated_power=3,
                    random_state=None)
 
 
 estimator.fit(factor_unit_trial)
 C_init = estimator.components_.T
-D_init = estimator.mean_
+D_init = estimator.mean_.reshape([-1, 1])
 
-Cs=[C_init.copy() for _ in range(K)]
-Ds=[D_init.copy().reshape([-1, 1]) for _ in range(K)]
 
-test_model = DefaultSLDS(K, yDim, xDim, inputDim,
-                         Cs=Cs,
-                         Ds=Ds)
+init_dynamics_distns = [Gaussian(nu_0=xDim+3,
+                                 sigma_0=3.*np.eye(xDim),
+                                 mu_0=np.zeros(xDim),
+                                 kappa_0=0.01)
+                        for _ in range(K)]
+
+
+dynamics_distns = [Regression(nu_0=xDim + 1,
+                              S_0=xDim * np.eye(xDim),
+                              M_0=np.hstack((.99 * np.eye(xDim), np.zeros((xDim, inputDim)))),
+                              K_0=xDim * np.eye(xDim + inputDim))
+                   for _ in range(K)]
+
+As = [np.eye(xDim) for _ in range(K)]
+if inputDim > 0:
+    As = [np.hstack((A, np.zeros((xDim, inputDim))))
+          for A in As]
+for dd, A in zip(dynamics_distns, As):
+    dd.A = A
+
+sigma_statess = [np.eye(xDim) for _ in range(K)]
+for dd, sigma in zip(dynamics_distns, sigma_statess):
+    dd.sigma = sigma
+
+emission_distns = [DiagonalRegression(yDim, xDim + inputDim,
+                                      mu_0=None, Sigma_0=None,
+                                      alpha_0=3.0, beta_0=2.0,
+                                      A=np.hstack((C_init.copy(), D_init.copy())),
+                                      sigmasq=None, niter=1)
+                   for _ in range(K)]
+
+
+train_model = HMMSLDS(
+    init_dynamics_distns= init_dynamics_distns,
+    dynamics_distns= dynamics_distns,
+    emission_distns= emission_distns,
+    alpha=3., init_state_distn='uniform')
+
 for trial in range(numTrial):
-    test_model.add_data(unit_trial[trial].T, inputs=inputs)
-    
+    train_model.add_data(unit_trial[trial].T, inputs=inputs)
+
+
 print("Initializing with Gibbs")
 N_gibbs_samples = 10
 def initialize(model):
     model.resample_model()
     return model.log_likelihood()
 
-gibbs_lls = [initialize(test_model) for _ in progprint_xrange(N_gibbs_samples)]
+gibbs_lls = [initialize(train_model) for _ in progprint_xrange(N_gibbs_samples)]
 
-# Fit with VBEM
+
 print("Fitting with VBEM")
 N_vbem_iters = 10
 def update(model):
     model.VBEM_step()
     return model.log_likelihood()
 
-test_model.states_list[0]._init_mf_from_gibbs()
-vbem_lls = [update(test_model) for _ in progprint_xrange(N_vbem_iters)]    
+train_model._init_mf_from_gibbs()
+vbem_lls = [update(train_model) for _ in progprint_xrange(N_vbem_iters)]
 
 
+
+test_model = train_model
+
+mask = np.ones((numTime, numUnit), dtype=bool)
+# leave out the last neuron
+mask[:,-1] = False
+# add the test data
+for trial in range(numTrial):
+    test_model.add_data(unit_trial[trial].T, inputs=inputs, mask=mask)
+
+test_states = test_model.states_list.pop()
+
+
+for intr in range(100):
+    test_states.resample()
+# get the latent states
+z_test = test_states.stateseq
+x_test = test_states.gaussian_states
+Cs = test_states.Cs
+Ds = test_states.Ds
+y_est = np.array([Cs[_,:,:].dot(x_test[_,:]).reshape(-1, 1)+Ds[_,:,:] for _ in range(numTime)]).squeeze()
